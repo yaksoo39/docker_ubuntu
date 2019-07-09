ecffa09fd1d45178559cdf5bc72efa44eea16894
NamJungGu <nowage@gmail.com>
2019-07-09 16:39:30 +0900

    First Commit

---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+apt update -y
+mkdir /home/$USER
+useradd -d /home/$USER $USER 
+chown $USER:$USER /home/$USER
+echo -e "$PASSWD\n$PASSWD" | passwd $USER
+echo  $USER password: $PASSWD
+# /bin/bash
+
